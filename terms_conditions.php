548450ce8013bfc7bae9fc0cdd29bf6ce5300cc4
pramodini23 <101609175+pramodini23@users.noreply.github.com>
2023-08-22 14:26:36 -0500

    Files

---
+++ terms_conditions.php
@@ -0,0 +1,160 @@
+<?php 
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="Gifts  eCommerce">
+	    <meta name="robots" content="all">
+
+	    <title>Order History</title>
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+		<!-- Demo Purpose Only. Should be removed in production -->
+		<link rel="stylesheet" href="assets/css/config.css">
+
+		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+	<script language="javascript" type="text/javascript">
+var popUpWin=0;
+function popUpWindow(URLStr, left, top, width, height)
+{
+ if(popUpWin)
+{
+if(!popUpWin.closed) popUpWin.close();
+}
+popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,copyhistory=yes,width='+600+',height='+600+',left='+left+', top='+top+',screenX='+left+',screenY='+top+'');
+}
+
+</script>
+
+	</head>
+    <body class="cnt-home">
+	
+		
+	
+		<!-- ============================================== HEADER ============================================== -->
+<header class="header-style-1">
+<?php include('includes/top-header.php');?>
+<?php include('includes/main-header.php');?>
+<?php include('includes/menu-bar.php');?>
+</header>
+<!-- ============================================== HEADER : END ============================================== -->
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="#">Home</a></li>
+				<li class='active'>Shopping Cart</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content outer-top-xs">
+	<div class="container">
+		<div class="row inner-bottom-sm">
+			<div class="shopping-cart">
+				<div class="col-md-12 col-sm-12 shopping-cart-table ">
+	<div class="table-responsive">
+<form name="cart" method="post">	
+
+
+			
+			<tbody>
+      <p><h2>Terms and Conditions<h2></p>
+
+	  <div style="font-size:12px;">
+	  a)Payment
+	   <p>
+While availing any of the payment method/s available on the site,FineArt & Gifts will not be responsible or assume any liability, whatsoever in respect of any loss or damage arising directly or indirectly to You due to:
+ <p>
+i)Lack of authorization for any transaction/s, or
+ <p>ii)Exceeding the preset limit mutually agreed by You and between "Bank/s", or
+ <p>iii)Any payment issues arising out of the transaction, or
+ <p>iv)Decline of transaction for any other reason/s
+ <p>v) All payments made against the purchases/services on site by you shall be compulsorily in Indian Rupees acceptable in the Republic of India. Site will not facilitate transaction with respect to any other form of currency with respect to the purchases made on Platform.
+
+ <p>
+ b) Excahnages & Replacement Guarantee:
+ <p>FineArt & Gifts will not be responsible for any item sold for replacement and exchanges.
+
+
+	 <p> Disclaimer: Pricing on any products in the site may due to some technical issue, typographical error or product information published  may be incorrectly reflected and in such an event seller may cancel such your orders.
+
+	  </div>
+			</tbody><!-- /tbody -->
+		</table><!-- /table -->
+		
+	</div>
+</div>
+
+		</div><!-- /.shopping-cart -->
+		</div> <!-- /.row -->
+		</form>
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+<?php echo include('includes/brands-slider.php');?>
+<!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	</div><!-- /.container -->
+</div><!-- /.body-content -->
+<?php include('includes/footer.php');?>
+
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	<script>
+		$(document).ready(function(){ 
+			$(".changecolor").switchstylesheet( { seperator:"color"} );
+			$('.show-theme-options').click(function(){
+				$(this).parent().toggleClass('open');
+				return false;
+			});
+		});
+
+		$(window).bind("load", function() {
+		   $('.show-theme-options').delay(2000).trigger('click');
+		});
+	</script>
+	<!-- For demo purposes – can be removed on production : End -->
+</body>
+</html>
